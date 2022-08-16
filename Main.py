d0dc6cc9605a6aeec370d359f572fbc735922795
Chris "Slothy <69122728+Im2Slothy@users.noreply.github.com>
2022-08-16 14:13:25 -0500

    Updated to include new Slash Command testing!

--- Main.py
+++ Main.py
@@ -13,10 +13,16 @@ import discord
 import typing
 from discord.ext import commands
 
+MY_GUILD = discord.Object(id=0)#INPUT GUILD ID 
+
+  async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
 
 client = commands.Bot(command_prefix = '.', intents = intents)
-
 
 
 #----------MEMBER JOIN AND LEAVE---------- BOT GOING ONLINE -----------------------------
@@ -75,9 +81,77 @@ async def on_message_edit(message_before, message_after):
 
 
 
+######### BETA TESTING ################ BETA TESTING ################ BETA TESTING #######
+#-----------------------------------SLASH COMMANDS---------------------------------------------
 
-#-----------------------------------------------------------------------------------------
+@client.tree.command()
+async def hello(interaction: discord.Interaction):
+    """Says hello!"""
+    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+    
+    
+@client.tree.command()
+@app_commands.describe(
+    first_value='The first value you want to add something to',
+    second_value='The value you want to add to the first value',
+)
+async def add(interaction: discord.Interaction, first_value: int, second_value: int):
+    """Adds two numbers together."""
+    await interaction.response.send_message(f'{first_value} + {second_value} = {first_value + second_value}')
 
+@client.tree.command()
+@app_commands.rename(text_to_send='text')
+@app_commands.describe(text_to_send='Text to send in the current channel')
+async def send(interaction: discord.Interaction, text_to_send: str):
+    """Sends the text into the current channel."""
+    await interaction.response.send_message(text_to_send)
+
+@client.tree.command()
+@app_commands.describe(member='The member you want to get the joined date from; defaults to the user who uses the command')
+async def joined(interaction: discord.Interaction, member: Optional[discord.Member] = None):
+    """Says when a member joined."""
+    # If no member is explicitly provided then we use the command user here
+    member = member or interaction.user
+
+    # The format_dt function formats the date time into a human readable representation in the official client
+    await interaction.response.send_message(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+# A Context Menu command is an app command that can be run on a member or on a message by
+# accessing a menu within the client, usually via right clicking.
+# It always takes an interaction as its first parameter and a Member or Message as its second parameter.
+
+# This context menu command only works on members
+@client.tree.context_menu(name='Show Join Date')
+async def show_join_date(interaction: discord.Interaction, member: discord.Member):
+    # The format_dt function formats the date time into a human readable representation in the official client
+    await interaction.response.send_message(f'{member} joined at {discord.utils.format_dt(member.joined_at)}')
+    
+
+# This context menu command only works on messages
+@client.tree.context_menu(name='Report to Moderators')
+async def report_message(interaction: discord.Interaction, message: discord.Message):
+    # We're sending this response message with ephemeral=True, so only the command executor can see it
+    await interaction.response.send_message(
+        f'Thanks for reporting this message by {message.author.mention} to our moderators.', ephemeral=True
+    )
+
+    # Handle report by sending it into a log channel
+    log_channel = interaction.guild.get_channel(0)  # replace with your channel id
+
+    embed = discord.Embed(title='Reported Message')
+    if message.content:
+        embed.description = message.content
+
+    embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
+    embed.timestamp = message.created_at
+
+    url_view = discord.ui.View()
+    url_view.add_item(discord.ui.Button(label='Go to Message', style=discord.ButtonStyle.url, url=message.jump_url))
+
+    await log_channel.send(embed=embed, view=url_view)
+
+######### BETA TESTING ################ BETA TESTING ################ BETA TESTING #######
 #------------------------------------Commands---------------------------------------------
 
 @client.command(help = "Tells you how many bitches you get... - Made for John")
